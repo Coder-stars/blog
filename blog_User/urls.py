aa9348f3f159dbb538aea4376330485062e5387c
coder_starts <475177575@qq.com>
2020-03-15 20:12:49 +0800

    feat:ç»å½æ³¨åå¾å®å

--- blog_User/urls.py
+++ blog_User/urls.py
@@ -3,5 +3,7 @@ from . import views
 app_name = 'blog_User'
 urlpatterns =[
     # path('',views.index),
-    path('login/',views.index)
+    path('login/',views.index),
+    path('register/',views.register),
+    path('forgetpwd/',views.get_pwd),
 ]
--- blog_User/views.py
+++ blog_User/views.py
@@ -5,3 +5,19 @@ from django.http import  HttpResponse,HttpResponseNotFound
 def index(request):
     return render(request,'login.html')
     # return  HttpResponse('用户首页')
+
+def register(request):
+    """
+    用户注册
+    :param request:
+    :return:
+    """
+    return  HttpResponse("用户注册")
+
+def get_pwd(requests):
+    """
+    找回密码
+    :param requests:
+    :return:
+    """
+    return HttpResponse("找回密码")
